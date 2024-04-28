e928855957faf648c73adc4bb24b7f5797b80b05
Connor Durkin <151471312+csdurkin@users.noreply.github.com>
2024-04-27 20:53:20 -0400

    Update helper.js
    Added: isValidZipcode,
      isValidLongitude,
      isValidLatitude,
      isValidPropertyCategory
    
    These are used in the property's branch, but I'm adding here to make merging easier down the line.

--- helper.js
+++ helper.js
@@ -1,30 +1,78 @@
 // valid strings
 export const isValidString = (str) => {
-if (typeof str !== 'string' || str.trim().length === 0) return false;
-return true;
+        if (typeof str !== 'string' || str.trim().length === 0) return false;
+        return true;
 };
 
 // valid email 
 export const isValidEmail = (email) => {
-const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,2}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-return re.test(String(email).toLowerCase());
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,2}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
 };
 
 // valid UUID format
 export const isValidUuid = (uuid) => {
-const re = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-return re.test(uuid);
+        const re = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+        return re.test(uuid);
 };
 
 // valid password (example: at least one number, one lowercase and one uppercase letter, at least six characters)
 export const isValidPassword = (password) => {
-const re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
-return re.test(password);
+        const re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+        return re.test(password);
 };
 
 // valid boolean type
 export const isBoolean = (bool) => {
-return typeof bool === 'boolean';
+        return typeof bool === 'boolean';
+};
+
+
+//Valid Zipcode
+export const isValidZipcode = (zipcode) => {
+    const re = /^\d{5}(?:-\d{4})?$/;
+    return re.test(zipcode);
+};
+
+
+//Valid Longitude
+export const isValidLongitude = (longitude) => {
+        
+    const parsedLongitude = parseFloat(longitude);
+        
+    if (isNaN(parsedLongitude)) {
+      return false;
+    }
+        
+    return parsedLongitude >= -180 && parsedLongitude <= 180;
+
+};
+
+
+//Valid Latitude
+export const isValidLatitude = (latitude) => {
+    
+    const parsedLatitude= parseFloat(latitude);
+    
+    if (isNaN(parsedLatitude)) {
+      return false;
+    }
+    return parsedLatitude >= -90 && parsedLatitude <= 90;
+
+};
+
+
+//Valid Property Category
+export const isValidPropertyCategory = (propertyCategory) => {
+    
+    const propertyCategories = ['Apartment', 'House', 'Condo', 'Townhouse', 'Studio'];
+
+    if (propertyCategories.includes(propertyCategory)){
+        return true;
+    } else {
+        return false;
+    }
+    
 };
 
 
@@ -79,10 +127,14 @@ const updateRating = async (landlordId) => {
 }
 
 export default {
-isValidString,
-isValidEmail,
-isValidUuid,
-isValidPassword,
-isBoolean, 
-updateRating
+  isValidString,
+  isValidEmail,
+  isValidUuid,
+  isValidPassword,
+  isBoolean,
+  isValidZipcode,
+  isValidLongitude,
+  isValidLatitude,
+  isValidPropertyCategory,
+  updateRating
 };
