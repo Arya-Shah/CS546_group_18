4ee903a132d93bd8f712ab1bb38494ac4d457897
Connor Durkin <151471312+csdurkin@users.noreply.github.com>
2024-05-02 18:10:06 -0400

    Update threads.js

--- data/threads.js
+++ data/threads.js
@@ -5,6 +5,54 @@ import { threads } from '../config/mongoCollections.js';
 import { v4 as uuid } from "uuid";
 import validators from "../helper.js";
 
+//Function: getAllThreads
+export const getAllThreads = async () => {
+
+    const threadCollection = await threads();
+    return await threadCollection.find({}).toArray();
+
+};
+
+// Function: getThreadById
+export const getThreadById = async (id) => {
+    
+    // Validation
+    if (!id || !validators.isValidUuid(id)) throw "Invalid ID input";
+
+    //Thread Collection and Thread
+    const threadCollection = await threads();
+    const thread = await threadCollection.findOne({ threadId: id });
+
+    // Validation (cont.)
+    if (!thread) throw "Thread not found";
+
+    // Return
+    return thread;
+};
+
+// Function: getThreadByCategory
+export const getThreadByCategory = async (category) => {
+   
+    // Validation
+    if (!category || !validators.isValidString(category) || category.trim().length === 0) {
+        throw "Invalid category input.";
+    }
+    category = category.trim().toLowerCase();
+
+    //Thread Collection
+    const threadCollection = await threads();
+
+
+    // Regular expression to make search case insensitive and global
+    const regex = new RegExp(category, 'gi');
+
+    // Find threads matching the category
+    const threadsMatchingCategory = await threadCollection.find({ category: regex }).toArray();
+
+    //Array of threads
+    return threadsMatchingCategory;
+};
+
 // Function: addThread
 export const addThread = async (
     userId,
@@ -26,7 +74,7 @@ export const addThread = async (
       if (!category || !validators.isValidString(category) || !validators.isValidThreadCategory(category) || category.trim().length === 0)
         throw "Invalid category input";
   
-      // Retrieve thread collection
+      // Thread Collection
       const threadCollection = await threads();
   
       // New Thread Object
@@ -50,10 +98,110 @@ export const addThread = async (
       // Validation (cont.)
       if (!insertInfo.acknowledged || !insertInfo.insertedId)
         throw "Could not add new thread";
-  
+
+      // Update User with thread id
+        const userCollection = await users();
+
+        const userUpdateStatus = await userCollection.updateOne(
+            { userId: userId },
+            { $push: { threadsIds: newThread.threadId } } 
+        );
+
+        if (!userUpdateStatus)
+            throw 'Failed to update user information with new thread.';
+    
       // Return
       return { threadInserted: true, threadId: newThread.threadId };
   };
+
+// Function: updateThread
+export const updateThread = async (threadId, updatedThread) => {
+    
+    // Validation
+    if (!threadId || !validators.isValidUuid(threadId)) 
+        throw "Invalid thread ID input";
+
+    if (!updatedThread || typeof updatedThread !== "object" || Array.isArray(updatedThread))
+        throw "Invalid updatedThread input";
+
+    //Thread collection
+    const threadCollection = await threads();
+
+    // Create object for updated thread information
+    const updatedThreadData = {};
+
+    // Validate and add updated thread information to object
+    if (updatedThread.title) {
+        if (!updatedThread.title || !validators.isValidString(updatedThread.title) || updatedThread.title.trim().length === 0)
+            throw "Invalid title input";
+        
+        updatedThreadData.title = updatedThread.title.trim();
+    }
+
+    if (updatedThread.threadText) {
+        if (!updatedThread.threadText || !validators.isValidString(updatedThread.threadText) || updatedThread.threadText.trim().length === 0)
+            throw "Invalid threadText input";
+        
+        updatedThreadData.threadText = updatedThread.threadText.trim();
+    }
+
+    if (updatedThread.category) {
+        if (!updatedThread.category || !validators.isValidString(updatedThread.category) || !validators.isValidThreadCategory(updatedThread.category) || updatedThread.category.trim().length === 0)
+            throw "Invalid category input";
+        
+        updatedThreadData.category = updatedThread.category.trim();
+    }
+
+    // Set lastUpdated to current date
+    updatedThreadData.lastUpdated = new Date();
+
+    // Update thread with object
+    const updateResponse = await threadCollection.updateOne(
+        { threadId: threadId },
+        { $set: updatedThreadData }
+    );
+
+    // Validation (cont.)
+    if (!updateResponse.acknowledged || updateResponse.modifiedCount === 0)
+        throw "Error occurred while updating thread";
+
+    // Return
+    return { threadUpdated: true };
+};
+
+// Function: removeThread
+export const removeThread = async (userId, threadId) => {
+    
+    // Validation
+    if (!threadId || !validators.isValidUuid(threadId)) throw "Invalid thread ID input";
+    if (!userId || !validators.isValidString(userId)) throw "Invalid userId input";
+    
+    // Retrieve thread collection
+    const threadCollection = await threads();
+    
+    // Delete thread
+    const deletionInfo = await threadCollection.deleteOne({ threadId: threadId });
+    
+    // Validation (cont.)
+    if (deletionInfo.deletedCount === 0) {
+        throw `Error occurred while deleting thread with ID ${threadId}`;
+    }
+
+    // Update User to remove thread id
+    const userCollection = await users();
+
+    const userUpdateStatus = await userCollection.updateOne(
+        { userId: userId },
+        { $pull: { threadsIds: threadId } } 
+    );
+
+    if (!userUpdateStatus)
+        throw 'Failed to update user information after adding new thread.';
+    
+    // Return
+    return { threadRemoved: true };
+};
+
 
 // Function: addCommentReply, adds comment to property or reply to comment
 export const addCommentReply = async (userId, threadOrCommentId, commentText) => {
@@ -123,7 +271,6 @@ export const addCommentReply = async (userId, threadOrCommentId, commentText) =>
         
 };
 
-
 // Function: removeCommentReply, removes comment from property or reply from comment
 export const removeCommentReply = async (userId, threadOrCommentId) => {
     
@@ -171,7 +318,6 @@ export const removeCommentReply = async (userId, threadOrCommentId) => {
         
 };
 
-
 //Function: addLikeDislike, adds like or dislike to comment or reply
 export const addLikeDislike = async (threadOrCommentId, likeOrDislike) => {
     
@@ -205,7 +351,6 @@ export const addLikeDislike = async (threadOrCommentId, likeOrDislike) => {
         );
 
     };
-
 
     //If failed, try to add like or dislike to reply
     if (!updateInfo.acknowledged || updateInfo.modifiedCount === 0){
